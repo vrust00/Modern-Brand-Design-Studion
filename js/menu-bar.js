a27cf88c454c557ea88d227417d4c16da0571d2c
Ivan Tumanov <vrust00@yandex.ru>
2026-06-10 21:51:45 +0400

    add menu-bar js logic

---
+++ js/menu-bar.js
@@ -0,0 +1,8 @@
+const menu_button = document.querySelector("#menu-button");
+const menu_bar = document.querySelector("#menu-bar");
+menu_button.addEventListener( 'click', () => {
+   
+      menu_bar.classList.toggle("hide");
+
+   
+});   
