f4c94bfacbd895a908a821fa3349e0cc1ad563c7
Ivan Tumanov <vrust00@yandex.ru>
2026-06-12 20:01:40 +0400

    add menu-bar logic

--- js/menu-bar.js
+++ js/menu-bar.js
@@ -1,8 +1,19 @@
 const menu_button = document.querySelector("#menu-button");
 const menu_bar = document.querySelector("#menu-bar");
 menu_button.addEventListener( 'click', () => {
-   
-      menu_bar.classList.toggle("hide");
+      
+
+    
+      if (menu_bar.classList.contains("hide")) {
+      menu_bar.classList.remove("hide");
+      } else if (window.innerWidth < 799) {
+      menu_bar.classList.add("hide");
+      }
+            
 
    
 });   
+
+window.addEventListener("resize", () => {
+      menu_bar.classList.add("hide");
+})
